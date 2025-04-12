15e6c762c5880ceece4c16d519ea28a250a6a22b
mrsaharg <winsahar5@gmail.com>
2025-04-12 15:36:11 +0500

    initial update

---
+++ app/src/main/java/com/example/myquote/module/quote.kt
@@ -0,0 +1,3 @@
+package com.example.myquote.module
+
+class Quote : ArrayList<QuoteItem>()
---
+++ app/src/main/java/com/example/myquote/module/quoteItem.kt
@@ -0,0 +1,7 @@
+package com.example.myquote.module
+
+data class QuoteItem(
+    val author: String,
+    val category: String,
+    val quote: String
+)
